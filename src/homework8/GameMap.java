17d28e46ea68c0e0b49a0b7e762846632da7a8e5
dewinor <kanopus05@mail.ru>
2022-08-02 00:08:56 +0700

    исправления

--- src/homework8/GameMap.java
+++ src/homework8/GameMap.java
@@ -1,5 +1,6 @@
 package homework8;
 
+import javax.management.loading.PrivateMLet;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
@@ -7,7 +8,7 @@ import java.awt.event.MouseEvent;
 import java.util.Random;
 
 public class GameMap extends JPanel {
-
+    private int STATE_MODE;
     public static final int MODE_HVSAI = 1;
     public static final int MODE_HVSHM = 2;
 
@@ -25,6 +26,16 @@ public class GameMap extends JPanel {
     private int cellWidth;
     private int cellHeight;
     private boolean initialMap;
+    private boolean isGameOver;
+
+    private int stateGameOver;
+    private static final int STATE_DRAW = 0;
+    private static final int STATE_WIN_HUMAN = 1;
+    private static final int STATE_WIN_AI = 2;
+
+    private static final String MSG_WIN_HUMAN = "Победил игрок!";
+    private static final String MSG_WIN_AI = "Победил компьютер!";
+    private static final String MSG_DRAW = "Ничья!";
 
     GameMap() {
         setBackground(Color.WHITE);
@@ -39,12 +50,14 @@ public class GameMap extends JPanel {
     }
 
     void startNewGame(int mode, int fieldSizeX, int fieldSizeY, int winLength) {
+        this.STATE_MODE = mode;
         this.fieldSizeX = fieldSizeX;
         this.fieldSizeY = fieldSizeY;
         this.winLength = winLength;
-        field = new int [fieldSizeX][fieldSizeY];
+        field = new int[fieldSizeX][fieldSizeY];
         initialMap = true;
         repaint();
+        isGameOver = false;
     }
 
     @Override
@@ -70,7 +83,7 @@ public class GameMap extends JPanel {
 
         for (int i = 0; i < fieldSizeY; i++) {
             int y = i * cellHeight;
-            graphics.drawLine(0,y,width,y);
+            graphics.drawLine(0, y, width, y);
         }
 
         for (int x = 0; x < fieldSizeX; x++) {
@@ -79,15 +92,42 @@ public class GameMap extends JPanel {
                     continue;
                 }
                 if (field[x][y] == HUMAN_DOT) {
-                    graphics.setColor(new Color(1, 1, 255));
-                    graphics.fillOval(x * cellWidth + PADDING_DOT, y * cellHeight + PADDING_DOT, cellWidth - PADDING_DOT * 2, cellHeight - PADDING_DOT * 2);
+                    graphics.setColor(new Color(227, 38, 54));
+                    Graphics2D g2 = (Graphics2D) graphics;
+                    g2.setStroke(new BasicStroke(3));
+                    g2.drawLine((x * cellWidth + PADDING_DOT), (y * cellHeight + PADDING_DOT), (x + 1) * cellWidth - PADDING_DOT, (y + 1) * cellHeight - PADDING_DOT);
+                    g2.drawLine((x + 1) * cellWidth - PADDING_DOT, (y * cellHeight + PADDING_DOT), (x * cellWidth + PADDING_DOT), (y + 1) * cellHeight - PADDING_DOT);
+
                 } else if (field[x][y] == AI_DOT) {
-                    graphics.setColor(Color.RED);
+                    graphics.setColor(new Color(73, 77, 78));
                     graphics.fillOval(x * cellWidth + PADDING_DOT, y * cellHeight + PADDING_DOT, cellWidth - PADDING_DOT * 2, cellHeight - PADDING_DOT * 2);
                 } else {
                     throw new RuntimeException("Error");
                 }
             }
+        }
+        if (isGameOver) {
+            showMessageGameOver(graphics);
+        }
+    }
+
+    private void showMessageGameOver(Graphics graphics) {
+        graphics.setColor(Color.GRAY);
+        graphics.fillRect(0, 185, getWidth(), 60);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(new Font("Montserrat", Font.BOLD, 25));
+        switch (stateGameOver) {
+            case STATE_WIN_HUMAN:
+                graphics.drawString(MSG_WIN_HUMAN, 130, getHeight() / 2);
+                break;
+            case STATE_WIN_AI:
+                graphics.drawString(MSG_WIN_AI, 110, getHeight() / 2);
+                break;
+            case STATE_DRAW:
+                graphics.drawString(MSG_DRAW, 180, getHeight() / 2);
+                break;
+            default:
+                throw new RuntimeException("Unexpected game parameter");
         }
     }
 
@@ -95,32 +135,40 @@ public class GameMap extends JPanel {
         if (!initialMap) {
             return;
         }
-
+        if (isGameOver) {
+            return;
+        }
         int cellX = e.getX() / cellWidth;
         int cellY = e.getY() / cellHeight;
 
-        if (!isValidCell(cellX, cellY) || !isEmptyCell(cellX, cellY)){
+        if (!isValidCell(cellX, cellY) || !isEmptyCell(cellX, cellY)) {
             return;
         }
         field[cellX][cellY] = HUMAN_DOT;
         if (checkWin(HUMAN_DOT)) {
-            repaint();
+            setGameOver(STATE_WIN_HUMAN);
             return;
         }
         if (isFullMap()) {
-            repaint();
+            setGameOver(STATE_DRAW);
             return;
         }
         aiTurn();
         repaint();
         if (checkWin(AI_DOT)) {
-            repaint();
+            setGameOver(STATE_WIN_AI);
             return;
         }
         if (isFullMap()) {
-            repaint();
+            setGameOver(STATE_DRAW);
             return;
         }
+    }
+
+    private void setGameOver(int gameOver) {
+        isGameOver = true;
+        stateGameOver = gameOver;
+        repaint();
     }
 
     private boolean isValidCell(int x, int y) {
@@ -131,14 +179,13 @@ public class GameMap extends JPanel {
         return field[x][y] == EMPTY_DOT;
     }
 
-
     private void aiTurn() {
-//        if (turnAIWinCell()) {
-//            return;
-//        }
-//        if (turnHumanWinCell()) {
-//            return;
-//        }
+        if (turnAIWinCell()) {
+            return;
+        }
+        if (turnHumanWinCell()) {
+            return;
+        }
         int x;
         int y;
         do {
@@ -151,7 +198,7 @@ public class GameMap extends JPanel {
     private boolean turnAIWinCell() {
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
-                if (isEmptyCell(x, y)) {
+                if (isEmptyCell(y, x)) {
                     field[y][x] = AI_DOT;
                     if (checkWin(AI_DOT)) {
                         return true;
@@ -166,7 +213,7 @@ public class GameMap extends JPanel {
     private boolean turnHumanWinCell() {
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
-                if (isEmptyCell(x, y)) {
+                if (isEmptyCell(y, x)) {
                     field[y][x] = HUMAN_DOT;
                     if (checkWin(HUMAN_DOT)) {
                         field[y][x] = AI_DOT;
@@ -199,7 +246,7 @@ public class GameMap extends JPanel {
         return false;
     }
 
-    private boolean checkLine(int x, int y, int vx, int vy, int winLen,int gameChip) {
+    private boolean checkLine(int x, int y, int vx, int vy, int winLen, int gameChip) {
         final int farX = x + (winLen - 1) * vx;
         final int farY = y + (winLen - 1) * vy;
         if (!isValidCell(farX, farY)) {
@@ -221,4 +268,5 @@ public class GameMap extends JPanel {
         }
         return true;
     }
+
 }
